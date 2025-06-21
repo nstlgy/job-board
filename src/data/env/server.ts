77704a85ef89cdab7256a92988f2a4c1fdd872c7
nstlgy <122057887+nstlgy@users.noreply.github.com>
2025-06-22 00:49:17 +0530

    initial setup

---
+++ src/data/env/server.ts
@@ -0,0 +1,14 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DB_PASSWORD: z.string().min(1),
+    DB_USER: z.string().min(1),
+    DB_HOST: z.string().min(1),
+    DB_PORT: z.string().min(1),
+    DB_NAME: z.string().min(1),
+  },
+  emptyStringAsUndefined: true,
+  experimental__runtimeEnv: process.env,
+});
