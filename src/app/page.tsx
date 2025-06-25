53990badcab5fe5753283469e7bc5d3adbe736b1
nstlgy <122057887+nstlgy@users.noreply.github.com>
2025-06-26 01:48:41 +0530

    Add app sidebar for client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,36 @@
-import { Sidebar, SidebarHeader, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebarClient } from "./_AppSidebarClient";
 
 export default function HomePage() {
   return (
     <SidebarProvider className="overflow-y-hidden">
-      <Sidebar collapsible="icon" className="overflow-hidden">
-        <SidebarHeader className="flex-row">
-          <SidebarTrigger />
-          <span className="text-xl text-nowrap">WDS JOBS</span>
-        </SidebarHeader>
-      </Sidebar>
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">WDS JOBS</span>
+          </SidebarHeader>
+          <SidebarContent></SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>Hello</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+      </AppSidebarClient>
+      <main className="flex-1">Hello</main>
     </SidebarProvider>
   );
 }
