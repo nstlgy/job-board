75215f66e76a9d8b2bdfdca9ee4738a9e844e1af
nstlgy <122057887+nstlgy@users.noreply.github.com>
2025-06-26 02:26:55 +0530

    wrapping up the snippet in suspense to pause rendering while waiting

--- src/services/clerk/components/ClerkProvider.tsx
+++ src/services/clerk/components/ClerkProvider.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import { ClerkProvider as OriginalClerkProvider } from "@clerk/nextjs";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import { dark } from "@clerk/themes";
 import { useIsDarkMode } from "@/hooks/useIsDarkMode";
 
 export function ClerkProvider({ children }: { children: ReactNode }) {
   const isDarkMode = useIsDarkMode();
+
   return (
-    <OriginalClerkProvider appearance={isDarkMode ? { baseTheme: [dark] } : undefined}>
-      {children}
-    </OriginalClerkProvider>
+    <Suspense>
+      <OriginalClerkProvider appearance={isDarkMode ? { baseTheme: [dark] } : undefined}>
+        {children}
+      </OriginalClerkProvider>
+    </Suspense>
   );
 }
